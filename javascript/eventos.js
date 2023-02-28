b9022c1c10e05a8fcb39a0463df3890d10e8cbd2
gastoncarballo <sgcarballo1978@gmail.com>
2023-02-27 23:05:17 -0300

    Commit proyecto

---
+++ javascript/eventos.js
@@ -0,0 +1,40 @@
+function enAmarillo1() {
+Element =document.getElementById("pers");
+Element.src="img/personal2.png";
+}
+function enNegro1() {
+    Element =document.getElementById("pers");
+    Element.src="img/personal.png";
+}
+function enAmarillo2() {
+    Element =document.getElementById("labo");
+    Element.src="img/laboral2.png";
+}
+ function enNegro2() {
+    Element =document.getElementById("labo");
+    Element.src="img/laboral.png";
+}
+function enAmarillo4() {
+    Element =document.getElementById("estu");
+    Element.src="img/estudios2.png";
+}
+function enNegro4() {
+    Element =document.getElementById("estu");
+    Element.src="img/estudios.png";
+}
+function enAmarillo6() {
+    Element =document.getElementById("gi");
+    Element.src="img/github2.png";
+}
+function enNegro6() {
+    Element =document.getElementById("gi");
+    Element.src="img/github.png";
+}
+function enAmarillo7() {
+    Element =document.getElementById("li");
+    Element.src="img/linkedin2.png";
+}
+function enNegro7() {
+    Element =document.getElementById("li");
+    Element.src="img/linkedin.png";
+}
